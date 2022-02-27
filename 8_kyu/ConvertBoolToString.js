47a076fbe65058e7e50c3528d2986798c97943d2
neur4l <lucasb.reis@hotmail.com>
2022-02-27 17:43:41 -0300

    Bool to Word working on Windows setup

---
+++ 8_kyu/ConvertBoolToString.js
@@ -0,0 +1,8 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+const boolToWord = (bool) => bool ? 'Yes' : 'No';
+
+console.log(boolToWord(true));
+console.log(boolToWord(false));
+console.log(boolToWord(1));
+console.log(boolToWord(0));
