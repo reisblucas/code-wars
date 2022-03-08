4392eedae9350c86e11d22ff1b6642d34c18d4bd
neur4l <lucasb.reis@hotmail.com>
2022-03-08 01:06:26 -0300

    Find Needle - 8 kyu - daily kata

---
+++ 8_kyu/findNeedle.js
@@ -0,0 +1,14 @@
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+// findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+// should return "found the needle at position 5" (in COBOL "found the needle at position 6")
+
+const findNeedle = (haystack) => `found the needle at position ${haystack.indexOf('needle')}`;
+
+console.log(findNeedle(['283497238987234', 'a dog', 'a cat', 'some random junk', 'a piece of hay', 'needle', 'something somebody lost a while ago']));
